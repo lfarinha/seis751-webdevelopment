17fd5395a4fc250697189d92990d0a2dece326b8
Leonardo Farinha <leonardofarinhap@gmail.com>
2017-11-18 14:19:53 -0600

    added php index and links

---
+++ assignments/php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="import" href="loader.html">
+    <link rel="icon" type="image/png" href="../icon/chelsea-converted.png">
+    <script src="../js/config.js" type="text/javascript"></script>
+</head>
+<body onload="setPageTitle()">
+  require 'links.php';
+</body>
+</html>
---
+++ assignments/php/links.php
@@ -0,0 +1,7 @@
+<nav class="menu">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="squad.html">Squad</a></li>
+        <li><a href="stadium.html">Stadium</a></li>
+    </ul>
+</nav>
