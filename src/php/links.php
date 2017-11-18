c77de48c91d72bdf8b2ad6210a469968bf081c6e
Leonardo Farinha <leonardofarinhap@gmail.com>
2017-11-18 16:00:32 -0600

    fixed links

--- src/php/links.php
+++ src/php/links.php
@@ -1,7 +1,7 @@
 <nav class="menu">
     <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="squad.html">Squad</a></li>
-        <li><a href="stadium.html">Stadium</a></li>
+        <li><a href="../html/index.html">Home</a></li>
+        <li><a href="../html/squad.html">Squad</a></li>
+        <li><a href="../html/stadium.html">Stadium</a></li>
     </ul>
 </nav>
