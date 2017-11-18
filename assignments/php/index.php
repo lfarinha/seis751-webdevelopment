93a7e7af04ad7623bb7f7a4c79741d2d4cec9ae1
Leonardo Farinha <leonardofarinhap@gmail.com>
2017-11-18 15:37:25 -0600

    fixed php tag missing

--- assignments/php/index.php
+++ assignments/php/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="import" href="loader.html">
+    <link rel="import" href="../html/loader.html">
     <link rel="icon" type="image/png" href="../icon/chelsea-converted.png">
     <script src="../js/config.js" type="text/javascript"></script>
 </head>
 <body onload="setPageTitle()">
-  require 'links.php';
+  <?php require 'links.php'; ?>
 </body>
 </html>
