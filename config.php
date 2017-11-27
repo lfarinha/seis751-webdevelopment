881665a8d0eedd6a219fc5635464dbf0cc4dfb8b
Leonardo Farinha <leonardofarinhap@gmail.com>
2017-11-26 18:57:37 -0600

    fixed path in config.php

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
   define("ROOT_PATH", $_SERVER["DOCUMENT_ROOT"]."/");
-  define("PROJECT_PATH", "/seis751-webdevelopment/src");
+  // define("PROJECT_PATH", "/seis751-webdevelopment/src");
+    define("PROJECT_PATH", "/assignments/src"); //only for UST SERVER
  ?>
