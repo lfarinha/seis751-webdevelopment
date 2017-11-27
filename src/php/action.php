2d3d4ddd4f9bc53e44a4787e4f71dc1fc23e609f
Leonardo Farinha <leonardofarinhap@gmail.com>
2017-11-26 18:49:56 -0600

    fixed paths in action.php

--- src/php/action.php
+++ src/php/action.php
@@ -1,5 +1,6 @@
 <?php
- require $_SERVER["DOCUMENT_ROOT"] . "/seis751-webdevelopment/config.php";
+ //require $_SERVER["DOCUMENT_ROOT"] . "/seis751-webdevelopment/config.php";
+ require $_SERVER["DOCUMENT_ROOT"] . "/assignments/config.php"; //only for UST SERVER
 
 if(isset($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['number_of_tickets']) and isset($_POST['date'])){
   $firstName = $_POST['first_name'];
@@ -8,7 +9,9 @@ if(isset($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['
   $date = $_POST['date'];
   $ticketNumber = rand(1000, 9999);
 
-  $request = ROOT_PATH . '/seis751-webdevelopment/tickets/' . $ticketNumber . '_' . $firstName . '_' . $lastName . '_' . $date . '_' . 'request.txt';
+  //$request = ROOT_PATH . '/seis751-webdevelopment/tickets/' . $ticketNumber . '_' . $firstName . '_' . $lastName . '_' . $date . '_' . 'request.txt';
+  $request = ROOT_PATH . '/assignments/tickets/' . $ticketNumber . '_' . $firstName . '_' . $lastName . '_' . $date . '_' . 'request.txt'; //only for UST SERVER
+
   $fp = fopen($request, 'w') or die('Cannot open file:  '.$request);
 
   fwrite($fp, "Ticket #: " . $ticketNumber . "\n\r");
